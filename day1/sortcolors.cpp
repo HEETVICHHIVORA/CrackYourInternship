21a25b337cfb6654476fd469cb49d1f16fa046ab
heetcodes <heetvic@gmail.com>
2024-07-14 20:11:37 +0530

    day1

---
+++ day1/sortcolors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int i =0,j=n-1;
+        int it = 0;
+
+        while(it<=j){
+            if(nums[it]==0){
+                swap(nums[it],nums[i]);
+                i++;
+                it++;
+
+            }
+           else if(nums[it]==2){
+                swap(nums[j],nums[it]);
+                j--;
+            }
+            else   it++;
+           
+
+        }
+    }
+};
